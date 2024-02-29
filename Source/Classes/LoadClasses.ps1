fc532d4974ed1d29bc62a44b8cfb48ac9d5b5be7
bab29 <brian.bors@cyberark.com>
2024-02-29 15:49:37 -0800

    Zip Upload

--- Source/Classes/LoadClasses.ps1
+++ Source/Classes/LoadClasses.ps1
@@ -1,3 +1,4 @@
+Add-Type -AssemblyName Microsoft.PowerShell.Commands.Utility
 $load = $null
 $(Get-childitem -path $PSScriptRoot -filter *.psm1).FullName|ForEach-Object {
 $load += "Using Module $PSItem`n"
